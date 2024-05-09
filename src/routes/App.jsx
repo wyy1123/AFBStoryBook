b9df43b79fe32925d8b6a06e85d8b907b23d5b9f
ianwAFB <ian1@afreebird.org>
2024-05-08 22:55:35 -0500

    fixed domain routing

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -12,8 +12,8 @@ function App() {
     <>
     <h1> Choose A Chapter</h1>
     <Card link = {'/App/Chapter1'} imgSrc={ch1Img} title= {'Chapter1'} text={'Begin the Journey with Alice'}/>
-    <Card link = {'App/Chapter2'} imgSrc={ch2Img} title= {'Chapter2'} text={'Alice enters the magical hallway'}/>
-    <Card link = {'App/Chapter3'} imgSrc={ch3Img} title= {'Chapter3'} text={'Alice in the Wonderland'}/> 
+    <Card link = {'/App/Chapter2'} imgSrc={ch2Img} title= {'Chapter2'} text={'Alice enters the magical hallway'}/>
+    <Card link = {'/App/Chapter3'} imgSrc={ch3Img} title= {'Chapter3'} text={'Alice in the Wonderland'}/> 
 
     </>
 
